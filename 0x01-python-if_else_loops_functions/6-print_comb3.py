50af2c1487aef9398afc7d2f6847bb6f3f3a1c38
sixolise <sixolisemabutiklaas@gmail.com>
2024-03-12 08:36:29 -0700

    Auto Push

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 10):
+for i in range(0, 9):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", ")
+        if i == 8 and j == 9:
+            print("{i}{j}".format(i=i, j=j))
+            break
+        print("{i}{j}".format(i=i, j=j), end=", ")
