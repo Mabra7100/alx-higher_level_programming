785368348caf2926d6635d1b1258fe87e2a7247b
sixolise <sixolisemabutiklaas@gmail.com>
2024-03-12 08:11:48 -0700

    Auto Push

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99+1):
+for i in range(00, 99+1):
     if i == 99:
         print("{i:d}".format(i=i))
         break;
-    print("{i};".format(i=i),end=" ")
+    print("{:02d};".format(i),end=" ")
